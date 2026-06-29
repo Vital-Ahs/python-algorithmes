e902bc251501f70ee93f6017df549f9403a9bb32
Gauis <gauisahissou@gmail.com>
2026-06-29 12:57:09 +0100

    Session 1 : variables et types

---
+++ session1.py
@@ -0,0 +1,26 @@
+prenom="Talïus"
+annee=2026
+moyenne=15.5
+print(prenom)
+print(annee)
+print(moyenne)
+# Vérifie les types
+print(type(prenom))
+print(type(annee))
+print(type(moyenne))
+
+# Concaténation — assembler du texte
+print("Je m'appelle " + prenom)
+
+# f-string — façon moderne et propre
+print(f"En {annee}, ma moyenne est {moyenne}")
+nom="Gaïus"
+filière="SIL"
+niveau=2
+bourse=False
+print(f"{nom} est en {filière}{niveau} à l'UATM.Boursier:{bourse}")
+print(type(nom))
+print(type(filière))
+print(type(niveau))
+print(type(bourse))
+
