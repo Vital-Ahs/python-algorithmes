d86faac4f3b15d95cefa47035bd70bc0e913a5df
Gauis <gauisahissou@gmail.com>
2026-06-29 14:53:37 +0100

    Session 3 : Boucles

---
+++ session3.py
@@ -0,0 +1,11 @@
+for i in range(1,11):
+    print(i)
+compteur = 10
+while compteur >= 1:
+    print(compteur)
+    compteur -= 1
+
+nbr = int(input("Entrer un nombre : "))
+for i in range(1, 11):
+    print(f"{nbr} x {i} = {nbr * i}")
+    
